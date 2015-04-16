ffaf4ccf138ace36e1361aa446d15f01d4b2f854
fangjun <fjun99@gmail.com>
2015-04-16 17:16:02 +0800

    first

--- eddwl-mini.php
+++ eddwl-mini.php
@@ -13,9 +13,12 @@ License: A "Slug" license name e.g. GPL2
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-if ( ! defined( 'EDD_WL_MINI_VERSION' ) )
-	define( 'EDD_WL_MINI_VERSION', '1.1' );
-
+function edd_wl_mini_print_scripts() {
+	if ( ! defined( 'EDD_WL_MINI_VERSION' ) ) {
+		define( 'EDD_WL_MINI_VERSION', '1.1' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'edd_wl_mini_print_scripts', 100 );
 
 // register scripts
 wp_register_script( 'edd-wl-mini', plugins_url( 'js/edd-wl-mini.js', __FILE__ ), array( 'jquery' ), EDD_WL_MINI_VERSION, true );
