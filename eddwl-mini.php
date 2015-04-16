e936b9f045d2a755e3455df6508e07ef2d9f40c9
fangjun <fjun99@gmail.com>
2015-04-16 18:49:05 +0800

    ajax

--- eddwl-mini.php
+++ eddwl-mini.php
@@ -13,34 +13,6 @@ License: A "Slug" license name e.g. GPL2
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-function edd_wl_mini_print_scripts() {
-
-	if ( ! defined( 'EDD_WL_MINI_VERSION' ) ) {
-		define( 'EDD_WL_MINI_VERSION', '1.0' );
-	}
-
-	// register scripts
-	wp_register_script( 'edd-wl-mini', plugins_url( '/js/edd-wl-mini.js', __FILE__ ), array( 'jquery' ), EDD_WL_MINI_VERSION, true );
-
-
-	wp_enqueue_script( 'ajax-script', plugins_url( '/js/my_query.js', __FILE__ ), array('jquery'),EDD_WL_MINI_VERSION, true  );
-
-	// in JavaScript, object properties are accessed as ajax_object.ajax_url, ajax_object.we_value
-	wp_localize_script( 'ajax-script', 'ajax_object',
-		array( 'ajax_url' => admin_url( 'admin-ajax.php' ), 'we_value' => 1234 ) );
-}
-add_action( 'wp_enqueue_scripts', 'edd_wl_mini_print_scripts', 100 );
-
-
-add_action( 'wp_ajax_my_action', 'my_action_callback' );
-add_action( 'wp_ajax_nopriv_my_action', 'my_action_callback' );
-function my_action_callback() {
-	global $wpdb;
-	$whatever = intval( $_POST['whatever'] );
-	$whatever += 10;
-	echo $whatever;
-	wp_die();
-}
 
 
 
