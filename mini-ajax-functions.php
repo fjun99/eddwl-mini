1f406bcf65c1dbc9914cb7785fb382ab5544f43b
fangjun <fjun99@gmail.com>
2015-04-16 18:50:41 +0800

    ajax

--- mini-ajax-functions.php
+++ mini-ajax-functions.php
@@ -5,3 +5,15 @@
  * Date: 15/4/16
  * Time: 下午4:50
  */
+
+
+add_action( 'wp_ajax_my_action', 'my_action_callback' );
+add_action( 'wp_ajax_nopriv_my_action', 'my_action_callback' );
+function my_action_callback() {
+	global $wpdb;
+	$whatever = intval( $_POST['whatever'] );
+	$whatever += 10;
+	echo $whatever;
+	wp_die();
+}
+
