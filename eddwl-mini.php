d15f0bc1bbcc31e914fdac35adb22839d39b84fe
fangjun <fjun99@gmail.com>
2015-04-16 18:43:06 +0800

    ajax

--- eddwl-mini.php
+++ eddwl-mini.php
@@ -20,10 +20,28 @@ function edd_wl_mini_print_scripts() {
 	}
 
 	// register scripts
-	wp_register_script( 'edd-wl-mini', plugins_url( 'js/edd-wl-mini.js', __FILE__ ), array( 'jquery' ), EDD_WL_MINI_VERSION, true );
+	wp_register_script( 'edd-wl-mini', plugins_url( '/js/edd-wl-mini.js', __FILE__ ), array( 'jquery' ), EDD_WL_MINI_VERSION, true );
 
+
+	wp_enqueue_script( 'ajax-script', plugins_url( '/js/my_query.js', __FILE__ ), array('jquery') );
+
+	// in JavaScript, object properties are accessed as ajax_object.ajax_url, ajax_object.we_value
+	wp_localize_script( 'ajax-script', 'ajax_object',
+		array( 'ajax_url' => admin_url( 'admin-ajax.php' ), 'we_value' => 1234 ) );
 }
 add_action( 'wp_enqueue_scripts', 'edd_wl_mini_print_scripts', 100 );
+
+
+add_action( 'wp_ajax_my_action', 'my_action_callback' );
+add_action( 'wp_ajax_nopriv_my_action', 'my_action_callback' );
+function my_action_callback() {
+	global $wpdb;
+	$whatever = intval( $_POST['whatever'] );
+	$whatever += 10;
+	echo $whatever;
+	wp_die();
+}
+
 
 
 require_once( dirname( __FILE__ ) . '/mini-ajax-functions.php' );
