1949425a373e50b8cc99cdf22b556844962a7866
fangjun <fjun99@gmail.com>
2015-04-16 17:06:21 +0800

    first

--- eddwl-mini.php
+++ eddwl-mini.php
@@ -9,3 +9,16 @@ Author: fangjun
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! defined( 'EDD_WL_MINI_VERSION' ) )
+	define( 'EDD_WL_MINI_VERSION', '1.1' );
+
+
+// register scripts
+wp_register_script( 'edd-wl-mini', plugins_url( 'js/edd-wl-mini.js', __FILE__ ), array( 'jquery' ), EDD_WL_MINI_VERSION, true );
+
+require_once( dirname( $this->file ) . 'mini-ajax-functions.php' );
+require_once( dirname( $this->file ) . 'mini-shortcodes.php' );
--- mini-shortcodes.php
+++ mini-shortcodes.php
@@ -5,3 +5,45 @@
  * Date: 15/4/16
  * Time: 下午4:50
  */
+
+
+/**
+ * Add to wish list shortcode
+ *
+ * @param  array $atts
+ * @param  $content
+ * @return object
+ * @since  1.0
+ */
+function edd_wl_add_to_list_mini_shortcode( $atts, $content = null ) {
+	global $post, $edd_options;
+/*
+	extract( shortcode_atts( array(
+			'id' 		=> $post->ID,
+			'text' 		=> ! empty( $edd_options[ 'edd_wl_add_to_wish_list' ] ) ? $edd_options[ 'edd_wl_add_to_wish_list' ] : __( 'Add to wish list', 'edd-wish-lists' ),
+			'icon'		=> $edd_options[ 'edd_wl_icon' ] ? $edd_options[ 'edd_wl_icon' ] : 'star',
+			'option'	=> 1, 		// default variable pricing option
+			'style'		=> $edd_options[ 'edd_wl_button_style' ] ? $edd_options[ 'edd_wl_button_style' ] : 'button',
+		), $atts, 'edd_wish_lists_add' )
+	);
+
+	$args = apply_filters( 'edd_wl_add_to_list_shortcode', array(
+		'download_id' 	=> $id,
+		'text' 			=> $text,
+		'icon'			=> $icon,
+		'style'			=> $style,
+		'action'		=> 'edd_wl_open_modal',
+		'class'			=> 'edd-wl-open-modal edd-wl-action before',
+		'price_option'	=> $option,
+		'shortcode'		=> true // used to return the links, not echo them in edd_wl_wish_list_link()
+	), $id, $option );
+
+	$content = edd_wl_wish_list_link( $args );
+*/
+	// load required scripts for this shortcode
+	wp_enqueue_script( 'edd-wl-mini' );
+
+	$content = "<p class='btn'>EDD WL MINI</p>";
+	return $content;
+}
+add_shortcode( 'edd_wish_lists_add_mini', 'edd_wl_add_to_list_mini_shortcode' );
