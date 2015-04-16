f3d1e4f9cf86f707c9c50b1b465ba3476ea30e1f
fangjun <fjun99@gmail.com>
2015-04-16 21:23:58 +0800

    ajax

--- eddwl-mini.php
+++ eddwl-mini.php
@@ -28,6 +28,15 @@ function edd_wl_mini_print_scripts() {
 	wp_localize_script( 'ajax-script', 'ajax_object',
 		array( 'ajax_url' => admin_url( 'admin-ajax.php' ), 'we_value' => 1234 ) );
 
+	wp_localize_script( 'edd-wl-mini', 'edd_wl_scripts', array(
+			'ajaxurl'                 => edd_get_ajax_url(),
+			'wish_list_page'          => edd_wl_get_wish_list_uri(),
+			'wish_list_add'           => edd_wl_get_wish_list_create_uri(),
+			'ajax_nonce'              => wp_create_nonce( 'edd_wl_ajax_nonce' ),
+		)
+	);
+
+
 }
 add_action( 'wp_enqueue_scripts', 'edd_wl_mini_print_scripts', 100 );
 
